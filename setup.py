3ea235f9e7323c773da46cd8b3486307ddd5c4c2
govindansriram <sriramgovindanwork@gmail.com>
2023-02-09 01:36:15 -0800

    edited setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='BenchScript',
+    python_requires='3.10.0',
     packages=['BenchScript'],
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='A mathematical based scripting language',
     long_description=long_description,
@@ -17,16 +18,16 @@ setuptools.setup(
     project_urls={  # Optional
         "Bug Tracker": "https://github.com/Bench-ai/BenchScript/issues"
     },
-    install_requires=["chicken==0.1.0;python_version==3.10.0",
-                      "dill==0.3.6;python_version==3.10.0",
-                      "egg == 0.2.0;python_version==3.10.0",
-                      "mpmath == 1.2.1;python_version==3.10.0",
-                      "numpy == 1.24.1;python_version==3.10.0",
-                      "sympy == 1.11.1;python_version==3.10.0"],
+    install_requires=["chicken==0.1.0",
+                      "dill==0.3.6",
+                      "egg == 0.2.0",
+                      "mpmath == 1.2.1",
+                      "numpy == 1.24.1",
+                      "sympy == 1.11.1"],
 
     keywords=["pypi", "BenchScript"],  # descriptive meta-data
     classifiers=[  # https://pypi.org/classifiers
-        'Development Status :: 2 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Documentation',
         'License :: OSI Approved :: MIT License',
